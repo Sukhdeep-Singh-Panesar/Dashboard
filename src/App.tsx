d73e8ca26e959cf9993275f6abfff6d75ee50a2a
Sukhdeep Singh <sukhdeepsingh1821@gmail.com>
2026-05-22 06:50:31 -0500

    fix border line issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css'
-
+import {Card} from "@/components/ui/card.tsx";
 function App() {
 
   return (
       <>
+          <Card className="App bg-pink-200 m-auto h-10 w-10" >
+              Hello
+          </Card>
       </>
   )
 }
