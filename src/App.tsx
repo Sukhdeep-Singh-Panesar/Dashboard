ef436bab9bbf3d6e43dffab30c2f5c2c16537e57
Sukhdeep Singh <sukhdeepsingh1821@gmail.com>
2026-06-07 22:24:05 -0500

    Sidebar created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import './App.css'
-import {Card} from "@/components/ui/card.tsx";
 function App() {
 
   return (
       <>
-          <Card className="App bg-pink-200 m-auto h-10 w-10" >
-              Hello Dashboard Setup Starts
-          </Card>
+
       </>
   )
 }
