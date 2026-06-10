5fcc9b32d3437dfce77a59f8c789192827e9d7f1
Sukhdeep Singh <sukhdeepsingh1821@gmail.com>
2026-06-09 21:05:05 -0500

    structure fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import './App.css'
 import Navbar from "@/components/Navbar.tsx";
+import Sidebar from "@/components/Sidebar";
 function App() {
 
   return (
       <>
-    <Navbar/>
+        <div className="flex h-screen overflow-hidden">
+            {/*sidebar*/}
+       <Sidebar/>
+
+            {/*Navbar*/}
+            <div className="flex-1 flex flex-col">
+          <Navbar/>
+
+            <main className="flex-1 overflow-auto p-6">
+                {/* main content area */}
+                <h1>Welcome to Dashboard</h1>
+            </main>
+            </div>
+        </div>
       </>
   )
 }
